c1bdc4ceba7cb50fcdd590909a085ae75826e55b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-07-02 20:00:23 +0300

    ContentType belongs to User, ContentType has many content items

--- app/Models/ContentType.php
+++ app/Models/ContentType.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ContentType extends Model
 {
-    //
+    protected $fillable = ['user_id', 'name'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function contentItems(): HasMany
+    {
+        return $this->hasMany(ContentItem::class);
+    }
 }
