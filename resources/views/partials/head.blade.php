778dcc06b4360b82076411612b584dcc0f9d3fa3
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-08-04 19:04:58 +0300

    set favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,22 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
+{{-- <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
+
+<!-- Favicon -->
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon16x16.png') }}">
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
+<!-- Apple Touch Icon -->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+
+<!-- Android Icons -->
+<link rel="icon" type="image/png" sizes="192x192" href="{{ asset('android-chrome-192x192.png') }}">
+<link rel="icon" type="image/png" sizes="512x512" href="{{ asset('android-chrome-512x512.png') }}">
+
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
