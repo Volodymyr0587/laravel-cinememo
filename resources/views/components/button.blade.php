097f2b78bf62a69df62b75675c8c68f6fe57ede8
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-08-07 22:15:49 +0300

    reduced padding by y axis on button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -5,7 +5,7 @@
 ])
 
 @php
-    $baseClasses = 'relative inline-flex items-center justify-center rounded-md px-5 py-2.5 font-semibold transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 hover:cursor-pointer';
+    $baseClasses = 'relative inline-flex items-center justify-center rounded-md px-5 py-2 font-semibold transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 hover:cursor-pointer';
 
     $colorStyles = [
         'green' => [
