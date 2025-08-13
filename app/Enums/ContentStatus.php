5721568b4866cb86dc6b2263adda6c8c3d1d6f75
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-08-13 20:13:43 +0300

    Add new content status Waiting

--- app/Enums/ContentStatus.php
+++ app/Enums/ContentStatus.php
@@ -7,6 +7,7 @@ enum ContentStatus: string
     case Watching = 'watching';
     case Watched = 'watched';
     case WillWatch = 'willwatch';
+    case Waiting = 'waiting';
 
     public static function values(): array
     {
@@ -19,6 +20,7 @@ enum ContentStatus: string
             self::Watching->value => 'Watching',
             self::Watched->value => 'Watched',
             self::WillWatch->value => 'Will Watch',
+            self::Waiting->value => 'Waiting',
         ];
     }
 }
