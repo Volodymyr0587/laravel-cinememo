6cfded3ee0bb56e6e4de798157efb306d1c350f7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-08-27 18:38:40 +0300

    Change app logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,8 +1,12 @@
-<div class="flex items-center gap-x-">
+{{-- <div class="flex items-center gap-x-">
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
         <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="ms-1 grid flex-1 text-start text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold">CineMemo</span>
     </div>
+</div> --}}
+
+<div class="flex items-center mr-1 sm:text-xl font-extrabold tracking-tight text-neon-red">
+    <span>🎥</span> {{ config('app.name', 'CineMemo') }}
 </div>
