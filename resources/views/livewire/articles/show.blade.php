5485c89eb083d75ddcbc2744708a5dd3e2603801
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-09-06 12:04:54 +0300

    feat(show article): change text color for dark mode for article author

--- resources/views/livewire/articles/show.blade.php
+++ resources/views/livewire/articles/show.blade.php
@@ -30,7 +30,7 @@
                 <div class="grow">
                     <div class="flex justify-between items-center gap-x-2">
                         <div class="flex flex-col">
-                            <div class="mb-1 text-gray-500 dark:text-neutral-500">{{ $article->user->name }}</div>
+                            <div class="mb-1 text-gray-500 dark:text-neutral-400">{{ $article->user->name }}</div>
                             <ul class="text-xs text-gray-500 dark:text-neutral-500">
                                 <li
                                     class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:size-1 before:bg-gray-300 before:rounded-full dark:text-neutral-400 dark:before:bg-neutral-600">
