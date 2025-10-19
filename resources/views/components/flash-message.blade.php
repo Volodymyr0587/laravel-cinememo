72e11d451d4dfe226a0630bf72abd130af4e0b73
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-10-19 17:26:56 +0300

    feat(ui): add glass-card class to flash-message component and increased timeout to 6 seconds

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,18 +1,18 @@
 @if (session()->has('message'))
-<div x-data="{show: true}" x-init="setTimeout(() => show = false, 4000)" x-show="show"
+<div x-data="{show: true}" x-init="setTimeout(() => show = false, 6000)" x-show="show"
     x-transition:enter="transition transform ease-out duration-300"
     x-transition:enter-start="translate-x-full opacity-0" x-transition:enter-end="translate-x-0 opacity-100"
     x-transition:leave="transition transform ease-in duration-300" x-transition:leave-start="translate-x-0 opacity-100"
     x-transition:leave-end="translate-x-full opacity-0" class="fixed top-4 right-4 z-50 w-[90%] max-w-md px-4">
     <div
-        class="border-l-8 border-green-500 bg-white text-gray-800 dark:bg-gray-600 dark:text-white shadow-lg rounded-md px-6 py-4">
+        class="glass-card px-6 py-4">
         <div class="flex justify-between items-center">
-            <p class="text-sm font-medium">
+            <p class="text-lg font-bold">
                 {{ session('message') }}
             </p>
             <button @click="show = false"
-                class="text-gray-400 hover:text-gray-600 dark:text-gray-800 dark:hover:text-gray-300 ml-4">
-                &times;
+                class="z-10 font-bold text-4xl text-gray-400 hover:text-gray-600 dark:text-gray-800 dark:hover:text-gray-300 ml-4 hover:cursor-pointer">
+                <flux:icon.x-circle  variant="solid" size="12" class="text-rose-500 dark:text-rose-300"/>
             </button>
         </div>
     </div>
